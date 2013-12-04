782efb40d34a015c552712db430cf75f3834eb25
Kjell Becker <k.becker@superreal.de>
2013-12-04 03:27:30 +0100

    Readd repos name to skin/theme install path.

--- src/Composer/Installers/OxidInstaller.php
+++ src/Composer/Installers/OxidInstaller.php
@@ -5,7 +5,7 @@ class OxidInstaller extends BaseInstaller
 {
     protected $locations = array(
         'module' => 'modules/{$name}/',
-        'theme'  => 'application/views/',
-        'out'    => 'out/',
+        'theme'  => 'application/views/{$name}/',
+        'out'    => 'out/{$name}/',
     );
 }
